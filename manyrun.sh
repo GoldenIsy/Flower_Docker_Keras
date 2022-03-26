164d76e72434072e45a7495a8d28fc5c43775950
GoldenIsy <102405994+GoldenIsy@users.noreply.github.com>
2022-03-26 09:07:37 +0100

    Add files via upload

--- manyrun.sh
+++ manyrun.sh
@@ -2,7 +2,7 @@
 
 server_name="server"
 
-docker run --rm --network flower -p 8085:8085 --name ${server_name} -v 'C:\Users\Manue\Desktop\docker\fishdata.pkl:/app' flwr_server_keras &
+docker run --rm --network flower -p 8080:8080 --name ${server_name} -v '<data>:/app' flwr_server_keras &
 sleep 15 # Sleep for 2s to give the server enough time to start
 
 clients=3
@@ -12,7 +12,7 @@ rounds=${clients--}
 
 for i in `seq 1 ${rounds}`; do
     echo "Starting client $i"
-    docker run --rm --network flower --name client${i} -v 'C:\Users\Manue\Desktop\docker\fishdata.pkl:/app' flwr_client_keras --partition=${i} --clients=${clients} --grpc_server_address=${server_name} &
+    docker run --rm --network flower --name client${i} -v '<data>:/app' flwr_client_keras --partition=${i} --clients=${clients} --grpc_server_address=${server_name} &
 done
 
 # This will allow you to use CTRL+C to stop all background processes
